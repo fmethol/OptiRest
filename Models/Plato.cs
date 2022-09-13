37ec6bf5487cb884bafb34a390e64e3f2a35f69e
Facundo <ezequiel-mu@live.com.arm>
2022-09-12 21:27:37 -0300

    Agregar archivos de proyecto.

---
+++ Models/Plato.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OptiRest.Models
+{
+    public class Plato
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        [Required]
+        public double Precio { get; set; }
+    }
+}
