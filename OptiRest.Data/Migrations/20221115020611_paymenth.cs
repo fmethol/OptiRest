e6cb816b7181f2114d9442e503d9f7648cfa9d39
Facundo <ezequiel-mu@live.com.ar>
2022-11-15 10:50:17 -0300

    migration paymenths

---
+++ OptiRest.Data/Migrations/20221115020611_paymenth.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OptiRest.Data.Migrations
+{
+    public partial class paymenth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                table: "TableServices",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentMethod",
+                table: "TableServices",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentReference",
+                table: "TableServices",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                table: "TableServices");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentMethod",
+                table: "TableServices");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentReference",
+                table: "TableServices");
+        }
+    }
+}
